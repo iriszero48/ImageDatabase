4530314f40fb499eed9bb304f91c0ee14096447c
iriszero48 <1026448790@qq.com>
2021-09-22 14:19:35 +0800

    v1.0.0.0

--- ImageDatabase/StdIO.h
+++ ImageDatabase/StdIO.h
@@ -2,6 +2,10 @@
 
 #include <iostream>
 
+#ifdef _MSC_VER
+#include <Windows.h>
+#endif
+
 namespace Console
 {
 	constexpr int Version[]{ 1, 0, 0, 0 };
@@ -19,16 +23,46 @@ namespace Console
 	{
 		static Color ForegroundColor = Color::White;
 
-		template<typename Stream, typename ...Args>
+		template<bool Line, typename Stream, typename ...Args>
 		void StreamCombine(Stream& stream, Args&&...args)
 		{
-			(stream << ... << args);
+			if constexpr (!Line) (stream << ... << args);
+			if constexpr (Line) (stream << ... << args) << std::endl;
 		}
 
-		template<typename Stream, typename ...Args>
-		void StreamCombineLine(Stream& stream, Args&&...args)
+		template<bool Line, typename Stream, typename ...Args>
+		void WriteImpl(Stream& stream, Args&&...args)
 		{
-			(stream << ... << args) << std::endl;
+#ifdef _MSC_VER
+			static std::unordered_map<Color, WORD> ColorMap
+			{
+				{ Color::Red, FOREGROUND_RED },
+				{ Color::Yellow, FOREGROUND_RED | FOREGROUND_GREEN },
+				{ Color::White, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN },
+				{ Color::Blue, FOREGROUND_BLUE },
+				{ Color::Gray, 0 },
+			};
+
+			auto* const console = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleTextAttribute(console, ColorMap.at(ForegroundColor) | FOREGROUND_INTENSITY);
+			StreamCombine<Line>(stream, std::forward<Args>(args)...);
+#else
+			if (__Detail::ForegroundColor == Color::White)
+			{
+				StreamCombine<Line>(stream, std::forward<Args>(args)...);
+				return;
+			}
+
+			static std::unordered_map<Color, std::string_view> ColorMap
+			{
+				{ Color::Red, "91" },
+				{ Color::Yellow, "93" },
+				{ Color::Blue, "94" },
+				{ Color::Gray, "90" },
+			};
+
+			StreamCombine<Line>(stream, "\x1B[", ColorMap.at(ForegroundColor), "m", std::forward<Args>(args)..., "\033[0m");
+#endif
 		}
 	}
 
@@ -40,30 +74,13 @@ namespace Console
 	template<typename ...Args>
 	void Write(Args&&...args)
 	{
-#ifdef _MSC_VER
-		__Detail::StreamCombine(std::cout, std::forward<Args>(args)...);
-#else
-		if (__Detail::ForegroundColor == Color::White)
-		{
-			__Detail::StreamCombine(std::cout, std::forward<Args>(args)...);
-			return;
-		}
-
-		static std::unordered_map<Color, std::string_view> ColorMap
-		{
-			{ Color::Red, "31" },
-			{ Color::Yellow, "33" },
-			{ Color::Blue, "34" },
-			{ Color::Gray, "30" },
-		};	
-
-#endif
+		__Detail::WriteImpl<false>(std::cout, std::forward<Args>(args)...);
 	}
 
 	template<typename ...Args>
 	void WriteLine(Args&&...args)
 	{
-		__Detail::StreamCombineLine(std::cout, std::forward<Args>(args)...);
+		__Detail::WriteImpl<true>(std::cout, std::forward<Args>(args)...);
 	}
 
 	namespace Error
@@ -71,13 +88,13 @@ namespace Console
 		template<typename ...Args>
 		void Write(Args&&...args)
 		{
-			__Detail::StreamCombine(std::cout, std::forward<Args>(args)...);
+			__Detail::WriteImpl<false>(std::cerr, std::forward<Args>(args)...);
 		}
 
 		template<typename ...Args>
 		void WriteLine(Args&&...args)
 		{
-			__Detail::StreamCombineLine(std::cerr, std::forward<Args>(args)...);
+			__Detail::WriteImpl<true>(std::cerr, std::forward<Args>(args)...);
 		}
 	}
 }
